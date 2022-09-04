4120defb5fc09278ddb94f648b04346605687cac
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-09-04 08:45:11 -0700

    Use new set_shader_parameter.

--- vignette_tunneling.gd
+++ vignette_tunneling.gd
@@ -7,25 +7,25 @@ var iris_depth_mat_inst: ShaderMaterial
 @export_range(3.0, 10.0, 0.01, "or_lesser", "or_greater") var iris_distance: float = 5.0:
 	set(x):
 		iris_distance = x
-		iris_color_mat_inst.set_shader_uniform(&"iris_distance", iris_distance)
-		iris_depth_mat_inst.set_shader_uniform(&"iris_distance", iris_distance)
+		iris_color_mat_inst.set_shader_parameter(&"iris_distance", iris_distance)
+		iris_depth_mat_inst.set_shader_parameter(&"iris_distance", iris_distance)
 
 @export_range(0.0, 1.0) var vignette_alpha: float = 1.0:
 	set(x):
 		vignette_alpha = x
-		iris_color_mat_inst.set_shader_uniform(&"vignette_alpha", vignette_alpha)
+		iris_color_mat_inst.set_shader_parameter(&"vignette_alpha", vignette_alpha)
 
 @export_range(0.0, 30.0, 0.1) var current_fade_fov: float = 6.5:
 	set(x):
 		current_fade_fov = x
-		iris_color_mat_inst.set_shader_uniform(&"current_fade_fov", current_fade_fov)
-		iris_depth_mat_inst.set_shader_uniform(&"current_fade_fov", current_fade_fov)
+		iris_color_mat_inst.set_shader_parameter(&"current_fade_fov", current_fade_fov)
+		iris_depth_mat_inst.set_shader_parameter(&"current_fade_fov", current_fade_fov)
 
 @export_exp_easing("attenuation") var current_fov: float = 100.0:
 	set(x):
 		current_fov = clamp(x, 0.0, 160.0)
-		iris_color_mat_inst.set_shader_uniform(&"current_fov", current_fov)
-		iris_depth_mat_inst.set_shader_uniform(&"current_fov", current_fov)
+		iris_color_mat_inst.set_shader_parameter(&"current_fov", current_fov)
+		iris_depth_mat_inst.set_shader_parameter(&"current_fov", current_fov)
 
 func _init():
 	var iris_mesh_const: Mesh = load("res://addons/xr_vignette/iris_extruded.obj") as Mesh
